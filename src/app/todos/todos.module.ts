60f8f9ef9b844f955947e55ce88d317728ccdd6d
Ramón Molinero <molinero.gomez.ramon@gmail.com>
2022-03-14 10:07:34 +0100

    creacion componentes

---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodosAddComponent } from './todos-add/todos-add.component';
+import { TodosFooterComponent } from './todos-footer/todos-footer.component';
+import { TodosItemComponent } from './todos-item/todos-item.component';
+import { TodosListComponent } from './todos-list/todos-list.component';
+import { TodosPageComponent } from './todos-page/todos-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    TodosAddComponent,
+    TodosFooterComponent,
+    TodosItemComponent,
+    TodosListComponent,
+    TodosPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TodosModule { }
